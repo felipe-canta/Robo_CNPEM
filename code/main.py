0ad413ee42856b11b99884957a5e3d4fc897a518
Daniel Wu <danielwu288@gmail.com>
2026-05-11 21:56:27 -0300

    planilha + codigo(ainda nao pronto)

--- code/main.py
+++ code/main.py
@@ -1,3 +1,4 @@
+from requests.sessions import default_headers
 import teste
 import math
 import requests
@@ -11,11 +12,15 @@ def enviar_comando(msg_do_teste):
     if len(partes) == 5:
         # Mapeia os valores para os nomes que o seu código Arduino (API) espera
         payload = {
-            "vA": partes[0],
-            "pA": partes[1],
-            "vX": partes[2],
-            "pX": partes[3],
-            "sA": partes[4],
+            "vX": partes[0],
+            "pX": partes[1],
+            "vY": partes[2],
+            "pY": partes[3],
+            "vZ": partes[4],
+            "pZ": partes[5],
+            "vA": partes[6],
+            "pA": partes[7],
+            "sA": partes[8],
         }
 
         try:
@@ -35,74 +40,80 @@ def enviar_comando(msg_do_teste):
         print(f"[ERRO] String inválida recebida do teste.py: '{msg_do_teste}'")
 
 
-def exec():
-    # Parâmetros Mecânicos
-    velocity = 500  # Velocidade padrão (passos/seg)
-    passo_volta = 1600  # Configuração do Driver (Microstepping)
-    wheel_diameter = 100  # mm
-    wheel_radius = wheel_diameter / 2
-    giro_const = 2.5  # Ajuste este valor se o robô girar mais ou menos que o pedido
-
-    # Cálculo de quantos passos equivalem a 1mm
-    perimetro = 2 * math.pi * wheel_radius
-    passo_por_mm = passo_volta / perimetro
-
+def get_position():
     while True:
-        print("\n" + "=" * 40)
-        print("      INTERFACE DE CONTROLE - CNPEM")
-        print("=" * 40)
-        print("1 - Mover em Reta (mm)")
-        print("2 - Girar Robô (graus)")
-        print("3 - Ajustar Velocidade")
-        print("4 - Abrir/Fechar Garra (Servo)")
-        print("5 - Modo Livre")
-        print("0 - Sair")
-
-        try:
-            opcao = input("\nEscolha uma opção: ")
-
-            if opcao == "0":
-                print("Encerrando sistema...")
-                break
-
-            elif opcao == "1":
-                dist = float(input("Distância para mover (mm): "))
-                # Chama a lógica modularizada
-                msg = teste.radial_movement(0, dist, velocity, passo_por_mm)
-                enviar_comando(msg)
-
-            elif opcao == "2":
-                deg = float(input("Ângulo para girar (graus - positivo=direita): "))
-                # Chama a lógica modularizada
-                msg = teste.tangent_movement(deg, velocity, giro_const)
-                enviar_comando(msg)
-
-            elif opcao == "3":
-                velocity = float(input(f"Velocidade atual é {velocity}. Nova: "))
-                print(f"Velocidade alterada para {velocity}")
-
-            elif opcao == "4":
-                estado = input("Garra: 1 para ABRIR, 0 para FECHAR: ")
-                # Envia comando parado (vel=0, passos=0) apenas para mexer o servo
-                msg = f"0 0 0 0 {estado}"
-                enviar_comando(msg)
-
-            elif opcao == "5":
-                servo, v1, p1, v2, p2 = input(
-                    "crie o um comando livre ( servo, velocidade1, passo1, velocidade2, passo2 ): "
-                ).split()
-                msg = f"{v1} {p1} {v2} {p2} {servo}"
-
-                enviar_comando(msg)
-
-            else:
-                print("Opção inválida!")
-
-        except ValueError:
-            print("Erro: Por favor, digite apenas números.")
-        except KeyboardInterrupt:
-            print("\nInterrompido pelo usuário.")
+        pos = input("digite a coordenada inicial (Xi Yi) [mm]: ").split()
+        result = list(map(int, pos))
+        if len(pos) == 2:
+            print(result)
+            return result
+        else:
+            print("digitar apenas 2 digitos")
             break
+
+
+def rad_movement(velocity, delta_r, dp):
+    vA = velocity
+    sA = int(delta_r / dp)
+    vX = vA
+    sX = sA
+
+    t = int(sA / vA)
+
+    query = f"{vA} {sA} 0 0 0 0 {vX} {sX} 0 0"
+
+    print(query)
+
+
+def tan_movement(velocity, g, j, dp, distancia_tan, delta_alpha, r_i):
+    sZ = int(delta_alpha * (r_i + g) / dp)
+    vZ = velocity
+
+    t = sZ / vZ
+
+    sY = int(-delta_alpha * (r_i - j) / dp)
+    vY = abs(int(sY / t))
+
+    sX = int(distancia_tan)
+    vX = abs(int(sX / t))
+
+    sA = sX
+    vA = vX
+
+    query = f"{vX} {sX} {vY} {sY} {vZ} {sZ} {vA} {sA} 0 0"
+
+    print(query)
+
+
+def exec():
+    diameter = 101.9
+    velocity = 400
+    passo_volta = 1600
+    dv = diameter * math.pi
+    dp = dv / passo_volta
+
+    x_raw, y_raw = get_position()
+    x_i, y_i = int(x_raw), int(y_raw)
+    r_i = math.sqrt(x_i**2 + y_i**2)
+    alpha_i = math.atan2(y_i, x_i)
+
+    x_raw, y_raw = get_position()
+    x_f, y_f = int(x_raw), int(y_raw)
+    r_f = math.sqrt(x_f**2 + y_f**2)
+    alpha_f = math.atan2(y_f, x_f)
+
+    delta_r = r_f - r_i
+    delta_alpha = alpha_f - alpha_i
+
+    g = 230
+    j = 130
+
+    distancia_tan = (g + j) * delta_alpha
+    passo_tan = distancia_tan / dv * passo_volta
+
+    rad_movement(velocity, delta_r, dp)
+
+    tan_movement(velocity, g, j, dp, distancia_tan, delta_alpha, r_i)
 
 
 if __name__ == "__main__":
